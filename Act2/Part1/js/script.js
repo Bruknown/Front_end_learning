722676bb4465781ec1c5eafb9ed53e02fa02f0eb
Bruknown <55330738+Bruknown@users.noreply.github.com>
2022-06-29 04:58:02 -0300

    Section 1 and 2 of programming course

---
+++ Act2/Part1/js/script.js
@@ -0,0 +1,25 @@
+// var resposta = confirm("Deseja excluir?");
+
+// console.log(resposta);
+
+const a = 10;
+const b = 33;
+
+function somar(a, b)
+{
+    return a + b;
+}
+
+document.querySelector("#calcular").addEventListener("click", function (){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+    if (valorA.length > 0 && valorB.length >0)
+    {
+        alert(parseInt(valorA) + parseInt(valorB));
+    }
+    else
+    {
+        alert("Digite valores");
+    }
+});
+// console.log(somar(a, b));
